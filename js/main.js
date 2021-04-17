f1cb1fea6ea3d06b11e48c850838741b7d66f5de
IT-T1ma <webdevtima@gmail.com>
2021-04-17 22:16:55 +0300

    Add new cicle and conditions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const numbersOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
+const numbersOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
 
-const  personalMovieDB = {
+const personalMovieDB = {
     count: numbersOfFilms,
     movies: {},
     actors: {},
@@ -10,13 +10,39 @@ const  personalMovieDB = {
     privat: false
 };
 
- let firstQuastionFilm = prompt("Один из последних просмотренных фильмов?");
- let firstRatingFilm = prompt("На сколько оцените его?");
+for (let i = 0; i < 2; i++) {
+    let quastionFilm = prompt("Один из последних просмотренных фильмов?");
+    let ratingFilm = prompt("На сколько оцените его?");
 
- let secondQuastionFilm = prompt("Один из последних просмотренных фильмов?");
- let secondRatingFilm = prompt("На сколько оцените его?");
+    if (quastionFilm != null && ratingFilm != null && quastionFilm.length < 50 && quastionFilm != "" && ratingFilm != "") {
+        personalMovieDB.movies[quastionFilm] = ratingFilm;
+        console.log('Good');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
 
-personalMovieDB.movies[firstQuastionFilm] = firstRatingFilm;
-personalMovieDB.movies[secondQuastionFilm] = secondRatingFilm;
-
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 console.log(personalMovieDB);
+
+for (let i = 0; i < 2; i++) {
+    let quastionFilm = prompt("Один из последних просмотренных фильмов?");
+    let ratingFilm = prompt("На сколько оцените его?");
+
+    if (quastionFilm != null && ratingFilm != null && quastionFilm.length < 50 && quastionFilm != "" && ratingFilm != "") {
+        personalMovieDB.movies[quastionFilm] = ratingFilm;
+        console.log('Good');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
